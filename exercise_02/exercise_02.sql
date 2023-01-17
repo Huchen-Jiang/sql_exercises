df99d599ad24c287c1375144a6b11900bd802d08
Huchen-Jiang <2437489168@qq.com>
2023-01-17 11:48:25 +0800

    Update exercise_02.sql

--- exercise_02/exercise_02.sql
+++ exercise_02/exercise_02.sql
@@ -55,3 +55,48 @@ SELECT * FROM departments
 WHERE Budget > (SELECT AVG(Budget) FROM departments);
 
 -- 14. Select the names of departments with more than two employees.
+SELECT NAME FROM departments
+WHERE Code IN (SELECT Department FROM employees GROUP BY Department HAVING COUNT(SSN) > 2);
+
+-- 15. Select the name and last name of employees working for departments with second lowest budget.
+-- Error Code: 1235. This version of MySQL doesn't yet support 'LIMIT & IN/ALL/ANY/SOME subquery'
+-- Solution: use a subquery to warp the subquery with LIMIT
+SELECT e.NAME, e.LastName, e.Department FROM employees e
+WHERE e.Department IN (
+SELECT temp.Code FROM(
+	SELECT d.Code FROM departments d
+    ORDER BY d.Budget ASC
+    LIMIT 2, 1
+) AS temp);
+
+-- 16. Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
+INSERT INTO departments(Code,Name,Budget)
+VALUE (11, "Quality Assurance", 40000);
+
+INSERT INTO employees(SSN,Name,LastName,Department)
+VALUE ("847219811", "Mary", "Moore", 11);
+
+-- 17. Reduce the budget of all departments by 10%.
+-- SET SQL_SAFE_UPDATES = 0;
+UPDATE departments
+SET Budget = Budget * 0.9;
+
+-- 18. Reassign all employees from the Research department (code 77) to the IT department (code 14).
+UPDATE employees
+SET Department = 14
+WHERE Department = 77;
+
+-- 19. Delete from the table all employees in the IT department (code 14).
+DELETE FROM employees
+WHERE Department = 14;
+
+-- 20. Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
+DELETE FROM employees
+WHERE Department IN (
+	SELECT Code FROM departments
+    WHERE Budget >= 60000
+);
+
+-- 21. Delete from the table all employees.
+DELETE FROM employees
+WHERE 1 = 1;
